bc81a82ab94d7e896b2047434d50aa7b6f173e1b
stef-snd <56920716+stef-snd@users.noreply.github.com>
2023-10-13 19:39:51 +0300

    Add files via upload
    lab1

---
+++ recombee.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from recombee_api_client.api_client import RecombeeClient, Region
+from recombee_api_client.exceptions import APIException
+from recombee_api_client.api_requests import *
+import csv
+import random
+private_token = "U5y6vlgvS6iRIbqE7SpwQ7KMBujtbJ56V4l4MOzCqYCY0pkk4bZLwJCtmAIRhCWs"
+client = RecombeeClient('upbss-dev',private_token,region=Region.EU_WEST)
+
+#add properties
+client.send(AddItemProperty('Domain Code','string'))
+client.send(AddItemProperty("Domain","string"))
+client.send(AddItemProperty("Item","string"))
+client.send(AddItemProperty("Description","string"))
+
+with open('dataset.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            # add ids 
+            # print(row[2])
+            client.send(AddItem(row[2]))
+            #add values 
+            client.send(SetItemValues(row[2],row[0], row[1], row[3]))
+            line_count += 1
+
+#add user, add user-item interactions 
+for user_id in range(5):
+  client.send(AddUser(user_id))
+  client.send(new recombee.AddPurchase(user_id, random.randint(400,3000)), callback);
+
+
+#recommend items to user 
+client.send(new recombee.RecommendItemsToUser(1, 4), callback);
+client.send(new recombee.RecommendItemsToUser(2, 3), callback);
